401c296fa1c84672dfbfd90bc2f2173075fd291c
Viperse <129846485+Viperse@users.noreply.github.com>
2023-08-03 17:43:09 +0900

    feat: Attribute, CafeMember

--- 03_HttpHeader/src/servlet/header/PrintHeaderServlet2.java
+++ 03_HttpHeader/src/servlet/header/PrintHeaderServlet2.java
@@ -42,8 +42,7 @@ public class PrintHeaderServlet2 extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		doGet(request, response);
+		doGet(request, response);
 	}
 
 }
